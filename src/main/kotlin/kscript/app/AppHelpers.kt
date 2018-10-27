fce71b41a2c86fa5f5a793d9a93867c65e7e6121
Egor Neliuba <eneliuba@gmail.com>
2018-10-27 23:23:21 +0300

    Add a failing test

--- src/main/kotlin/kscript/app/AppHelpers.kt
+++ src/main/kotlin/kscript/app/AppHelpers.kt
@@ -1,7 +1,11 @@
 package kscript.app
 
 import kscript.app.ShellUtils.requireInPath
-import java.io.*
+import java.io.BufferedReader
+import java.io.File
+import java.io.IOException
+import java.io.InputStream
+import java.io.InputStreamReader
 import java.net.URL
 import java.nio.file.Files
 import java.nio.file.Paths
