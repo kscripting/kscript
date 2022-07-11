4b8ac1b91762111f2c169f5d605bad5a0cee9fa3
Marcin <aarti@interia.pl>
2022-07-11 17:07:14 +0200

    Fixes for MSYS.

--- test/linux_suite/idea.sh
+++ test/linux_suite/idea.sh
@@ -1,12 +1,12 @@
 ## temp projects with include symlinks
 TMP_DIR=$(kscript --idea ${PROJECT_DIR}/test/resources/includes/include_variations.kts)
 assert_raises "echo $?" 0
-assert_raises "cd $TMP_DIR && gradle build" 0
+assert_raises "cd '$TMP_DIR' && gradle build" 0
 
 ## support diamond-shaped include schemes (see #133)
 TMP_DIR=$(kscript --idea ${PROJECT_DIR}/test/resources/includes/diamond.kts)
 assert_raises "echo $?" 0
-assert_raises "cd $TMP_DIR && gradle build" 0
+assert_raises "cd '$TMP_DIR' && gradle build" 0
 
 ## todo re-enable interactive mode tests using kscript_nocall
 #kscript_nocall() { kotlin -classpath ${PROJECT_DIR}/build/libs/kscript.jar kscript.app.KscriptKt "$@";}
