a384bd6151f6832e3ff0de454f67fdcf82f63c7b
Marcin Kuszczak <1508798+aartiPl@users.noreply.github.com>
2022-09-21 01:08:58 +0200

    * Fixed unit test
    Signed-off-by: Marcin Kuszczak <1508798+aartiPl@users.noreply.github.com>

--- src/test/kotlin/io/github/kscripting/kscript/util/LoggerTest.kt
+++ src/test/kotlin/io/github/kscripting/kscript/util/LoggerTest.kt
@@ -11,21 +11,21 @@ class LoggerTest {
     private val exception2 = IllegalStateException("This is some sophisticated exception message.")
 
     @Test
-    fun `test error message for exception`() {
+    fun `Test error message for exception`() {
         Logger.devMode = false
         Logger.silentMode = false
         assertThat(errorMsg(exception1)).isEqualTo("[kscript] [ERROR] Test message\n[kscript] [ERROR] Second line")
     }
 
     @Test
-    fun `test stacktrace for exception`() {
+    fun `Test stacktrace for exception`() {
         Logger.devMode = true
         Logger.silentMode = false
         assertThat(errorMsg(exception2)).startsWith(
             """ |[kscript] [ERROR] This is some sophisticated exception message.
                 |[kscript] [ERROR] 
                 |[kscript] [ERROR] java.lang.IllegalStateException: This is some sophisticated exception message.
-                |[kscript] [ERROR] 	at io.github.kscripting.kscript.util.LoggerTest.<init>(LoggerTest.kt:10)""".trimMargin()
+                |[kscript] [ERROR] 	at io.github.kscripting.kscript.util.LoggerTest.<init>""".trimMargin()
         )
     }
 }
