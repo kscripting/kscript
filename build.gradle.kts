d5720d26033680a888cf10c066246ea34fc1b8c4
Marcin Kuszczak <1508798+aartiPl@users.noreply.github.com>
2022-11-25 20:41:33 +0100

    Updated shell to release version.
    Signed-off-by: Marcin Kuszczak <1508798+aartiPl@users.noreply.github.com>

--- build.gradle.kts
+++ build.gradle.kts
@@ -141,7 +141,7 @@ dependencies {
     implementation("net.igsoft:tablevis:0.6.0")
     implementation("io.arrow-kt:arrow-core:1.1.2")
 
-    implementation("io.github.kscripting:shell:0.5.0-SNAPSHOT")
+    implementation("io.github.kscripting:shell:0.5.0")
 
     implementation("org.slf4j:slf4j-nop:2.0.4")
 
