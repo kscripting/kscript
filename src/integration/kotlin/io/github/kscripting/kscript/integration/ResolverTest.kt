6dcf324cb476ad6fddd087a3a5c6b99f0136611d
Marcin Kuszczak <1508798+aartiPl@users.noreply.github.com>
2022-11-26 20:33:10 +0100

    Fixed test.
    Signed-off-by: Marcin Kuszczak <1508798+aartiPl@users.noreply.github.com>

--- src/integration/kotlin/io/github/kscripting/kscript/integration/ResolverTest.kt
+++ src/integration/kotlin/io/github/kscripting/kscript/integration/ResolverTest.kt
@@ -9,21 +9,28 @@ import org.junit.jupiter.api.Test
 import java.io.File
 
 class ResolverTest : TestBase {
-
     @Test
-    @Tag("linux")
-    @Tag("macos")
-    @Tag("msys")
+    @Tag("posix")
     @Tag("windows")
-    fun `it should run kscript and resolve dependencies`() {
+    fun `It should run kscript and resolve dependencies`() {
+        // The first time artifact resolution is started because the cache is cleaned...
+        verify(
+            "kscript ${resolvePath("$projectDir/test/resources/depends_on_annot.kts")}",
+            0,
+            "kscript with annotations rocks!\n",
+            startsWith("[kscript] Resolving log4j:log4j:1.2.14")
+        )
+
+
         // clear .m2 cache
         val log4jCached = File(System.getProperty("user.home"), ".m2/repository/log4j/log4j/1.2.14/")
 
         if (log4jCached.isDirectory) {
-            System.err.println("Cleaning up cached .m2 copy of log4j")
+            System.err.println("Cleaning up cached copy of log4j: ${log4jCached.absolutePath}")
             log4jCached.deleteRecursively()
         }
 
+        // The second time it is because of removing artifact from cache...
         verify(
             "kscript ${resolvePath("$projectDir/test/resources/depends_on_annot.kts")}",
             0,
